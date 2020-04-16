c81b3cbf07b5923f7d9f2408749b35d3bea2b084
Allison Darwin <allisonpalmer07@gmail.com>
2020-04-16 11:55:34 -0400

    add child theme for edits

---
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Accelerate Marketing Child functions and definitions
+*
+* @link http://codex.wordpress.org/Theme_Development
+* @link http://codex.wordpress.org/Child_Themes
+*
+* @package WordPress
+* @subpackage Accelerate Marketing
+* @since Accelerate Marketing 2.0
+*/
+
+// Enqueue scripts and styles
+function accelerate_child_scripts(){
+	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' ); //get_template_directory_uri() function references theme files in parent theme, which we want to enque first
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' )); //get_stylesheet_directory_uri() points to active theme, which is the child in this case
+}
+add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
