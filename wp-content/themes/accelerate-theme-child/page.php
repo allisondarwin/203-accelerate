25362bb410c1007a9e611a8f55e3b3fa285d5b4c
Allison Darwin <allisonpalmer07@gmail.com>
2020-04-30 12:46:06 -0400

    add title tag and remove sidebar

--- wp-content/themes/accelerate-theme-child/page.php
+++ wp-content/themes/accelerate-theme-child/page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying the contact page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -17,11 +17,10 @@ get_header(); ?>
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
-
-		<?php get_sidebar(); ?>
 
 	</div><!-- #primary -->
 
